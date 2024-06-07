a48cdc745ff83c3fac2d42d201abb88b88465ce3
rigvyaspro <146059700+rigvyaspro@users.noreply.github.com>
2024-06-07 10:12:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ print("Welcome to the Band Name Generator")
 city = input("What's the name of city you were born in? ")
 pet = input("What's the name of your pet? ")
 
-print("your band name could be " + city + "'s" + pet)
+print("your band name could be " + city + "'s " + pet)
 
